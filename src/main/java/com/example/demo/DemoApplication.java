d5ddd9fa5097d57995f1dee7121ab7797fe31b5b
谢泽浩 <6579478+xie_se_hao@user.noreply.gitee.com>
2021-11-16 11:53:27 +0800

    hotfix commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 		System.out.println(111);
 		System.out.println(222);
 		System.out.println(333);
-		System.out.println("master commit");
+		System.out.println("hotfix commit");
 	}
 
 }
